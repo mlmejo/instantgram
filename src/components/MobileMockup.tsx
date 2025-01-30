678c7b56de2e781e6e7a980072c0df9b87430c9f
mlmejo <mlmejo12@gmail.com>
2025-01-30 17:04:03 +0800

    Add documentation for MobileMockup

--- src/components/MobileMockup.tsx
+++ src/components/MobileMockup.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 
 export default function MobileMockup() {
+  // Use an array to hold the preview images to display
+  // on the mobile mockup.
   const screenshots = [
     'screenshot1-2x.png',
     'screenshot2-2x.png',
@@ -10,6 +12,10 @@ export default function MobileMockup() {
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  // The logic of the presentation is as follows:
+  // During component mount, create an interval which
+  // changes the current index state to show a different
+  // image.
   useEffect(() => {
     const intervalId = setInterval(() => {
       setCurrentIndex((prev) => {
@@ -28,11 +34,16 @@ export default function MobileMockup() {
   return (
     <div className="mb-3 mr-8 hidden h-[581.146px] w-full basis-[380.32px] self-center bg-[url(/home-phones-2x.png)] bg-[length:468.32px_634.15px] [background-position-x:-3rem] lg:block">
       <div className="relative left-[112px] top-[27px]">
+        {/*
+          Mapping all the images is needed in order to create transitions.
+          As changing the `src` of the image will not allow us to do transitions.
+        */}
         {screenshots.map((screenshot, index) => (
           <img
             key={index}
             src={`/screenshots/${screenshot}`}
             alt=""
+            // The important part is to hide the non-active images.
             className={
               `absolute left-0 top-0 h-[538.84px] w-[250px] overflow-clip align-baseline ` +
               (currentIndex === index
